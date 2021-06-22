cd9fb8d4a6dc4bf90099e25b7032ad138a8a961f
Kayane Vieira <kayane@startaideia.com.br>
2021-06-22 10:46:52 -0300

    more exercises done

--- 01_Day_Introduction/exercises/level_1.js
+++ 01_Day_Introduction/exercises/level_1.js
@@ -1,1 +1,11 @@
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
 const challenge = "30 Days of Javascript"
+
+//Print the string on the browser console using console.log()
+
+console.log(challenge)
+
+//Print the length of the string on the browser console using console.log()
+
+console.log(challenge.length)
