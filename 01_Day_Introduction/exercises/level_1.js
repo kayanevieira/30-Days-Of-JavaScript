db243912bb85f55e9ed8a1edc09c881e1c876257
Kayane Vieira <kayane@startaideia.com.br>
2021-06-22 10:59:15 -0300

    add slice string exercise

--- 01_Day_Introduction/exercises/level_1.js
+++ 01_Day_Introduction/exercises/level_1.js
@@ -1,15 +1,20 @@
 //Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
-
 const challenge = "30 Days of Javascript"
 
 //Print the string on the browser console using console.log()
-
 console.log(challenge)
 
 //Print the length of the string on the browser console using console.log()
-
 console.log(challenge.length)
 
 //Change all the string characters to capital letters using toUpperCase() method
-var stringBigger = challenge.toUpperCase()
-console.log(stringBigger)
+var BiggerString = challenge.toUpperCase()
+console.log(BiggerString)
+
+//Change all the string characters to lowercase letters using toLowerCase() method
+let ShorterString = challenge.toLowerCase()
+console.log(ShorterString)
+
+//Cut (slice) out the first word of the string using substr() or substring() method
+let SliceString = challenge.substr(1)
+console.log(SliceString)
