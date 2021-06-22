75ef2bcde0f8058b2786fac35900d291dc423874
Kayane Vieira <kayane@startaideia.com.br>
2021-06-22 15:38:31 -0300

    add exercise search()

--- 01_Day_Introduction/exercises/level_1.js
+++ 01_Day_Introduction/exercises/level_1.js
@@ -65,3 +65,6 @@ console.log(Sentence.indexOf("because"))
 
 //Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 console.log(Sentence.lastIndexOf("because"))
+
+//Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(Sentence.search("because"))
