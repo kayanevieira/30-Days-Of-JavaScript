fc1db3c9069dfa1ec0cd88c3492e41fcd744bd6b
Kayane Vieira <kayane@startaideia.com.br>
2021-06-22 15:50:41 -0300

    add exercise endsWith()

--- 01_Day_Introduction/exercises/level_1.js
+++ 01_Day_Introduction/exercises/level_1.js
@@ -75,3 +75,6 @@ console.log(ChallengeSpace.trim())
 
 //Use startsWith() method with the string 30 Days Of JavaScript and make the result true
 console.log(challenge.startsWith("30"))
+
+//Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.endsWith('Javascript'))
