6570415da48dbf0c479d7f4518f774ed3786c082
Kayane Vieira <kayane@startaideia.com.br>
2021-06-24 09:53:09 -0300

    add exercise typeof

--- 01_Day_Introduction/exercises/level_2.js
+++ 01_Day_Introduction/exercises/level_2.js
@@ -4,6 +4,13 @@ console.log('\'There is no exercise better for the heart than reaching down and 
 // 02- Using console.log() print out the following quote by Mother Teresa:
 console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
 
+// 03- Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+if (typeof '10' == 10){
+    console.log("true")
+}else{
+    console.log(typeof Number('10'), 'equal now')
+}
+
 
 
 
