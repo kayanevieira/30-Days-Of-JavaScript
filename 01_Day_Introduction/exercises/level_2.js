eccc3f2f86aba58b473cf3d04a4a3cf319ffc3eb
Kayane Vieira <kayane@startaideia.com.br>
2021-06-24 11:30:21 -0300

    add exercise 11

--- 01_Day_Introduction/exercises/level_2.js
+++ 01_Day_Introduction/exercises/level_2.js
@@ -56,4 +56,12 @@ function generateLetter (){
 
 console.log(randomNumber,' = ', generateLetter())
 
-//Use console.log() and escape characters to print the following pattern.
+// 11-Use console.log() and escape characters to print the following pattern.
+    //1 1 1 1 1
+    //2 1 2 4 8
+    //3 1 3 9 27
+    //4 1 4 16 64
+    //5 1 5 25 125
+
+console.log('\n 1 1 1 1 1\n 2 1 2 4 8\n 3 1 3 9 27\n 4 1 4 16 64\n 5 1 5 25 125');
+
