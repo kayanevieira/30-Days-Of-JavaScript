771b1b50f08173a1e8c0ad687463a85905b7d6a8
Kayane Vieira <kayane@startaideia.com.br>
2021-06-24 09:52:30 -0300

    add another exercise console.log()

--- 01_Day_Introduction/exercises/level_2.js
+++ 01_Day_Introduction/exercises/level_2.js
@@ -1,7 +1,8 @@
 // 01- Using console.log() print out the following statement:
 console.log('\'There is no exercise better for the heart than reaching down and lifting people up.\'')
 
-
+// 02- Using console.log() print out the following quote by Mother Teresa:
+console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
 
 
 
