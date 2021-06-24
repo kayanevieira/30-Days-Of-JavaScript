3f04b83b4e6680d7eeb90d6550f6337f2969f844
Kayane Vieira <kayane@startaideia.com.br>
2021-06-24 13:23:57 -0300

    add exercise 1

---
+++ 01_Day_Introduction/exercises/level_3.js
@@ -0,0 +1,12 @@
+// 01 -'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+const loveSentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+let numberOfTimes = (loveSentence.match(/love/gi))
+
+for (i = 0; i < numberOfTimes.length; i++){
+    console.log(i)
+}
+
+
+
+
+
