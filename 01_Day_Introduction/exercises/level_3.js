5e5de2162c9b8b201a2ff1db01f57ed28100227e
Kayane Vieira <kayane@startaideia.com.br>
2021-06-25 10:44:23 -0300

    add exercise 4

--- 01_Day_Introduction/exercises/level_3.js
+++ 01_Day_Introduction/exercises/level_3.js
@@ -11,6 +11,22 @@ const becauseSentence = 'You cannot end a sentence with because because because 
 const countTime = becauseSentence.match(/because/gi)
 console.log(countTime.length)
 
+// 03- Clean the following text and find the most frequent word (hint, use replace and regular expressions).const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+
+var text =  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching"
+		const removing = /[%@&$#;']/gi;
+		text = text.replace(removing, "");
+		console.log(text);
+    //find the most frequency
+	
 
 
 
+// 04- Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+const salary = 5000 
+const annualBonus = 1000
+const onlineCourses = 15000
+
+let sum = (salary + annualBonus + onlineCourses)
+let total = (sum * 12)
+console.log(total, 'per year')
