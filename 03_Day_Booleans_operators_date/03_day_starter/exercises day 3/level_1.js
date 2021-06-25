51ef430ffc89093a880bd5ea91ccacbd9e7d4ed5
Kayane Vieira <kayane@startaideia.com.br>
2021-06-25 10:56:12 -0300

    add exercise 1

---
+++ 03_Day_Booleans_operators_date/03_day_starter/exercises day 3/level_1.js
@@ -0,0 +1,18 @@
+//01- Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+const firstName = 'Kayane';
+const lastName = 'Vieira';
+const country = 'Brasil';
+const city = 'Porto Alegre';
+const age = 18;
+const isMarried = false;
+const year = 2021;
+
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
+
+//02- Check if type of '10' is equal to 10
