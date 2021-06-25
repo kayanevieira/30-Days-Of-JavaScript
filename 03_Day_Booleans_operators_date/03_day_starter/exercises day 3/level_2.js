2b3875aa86354e621f616d4ea8fd31eb22fd7284
Kayane Vieira <kayane@startaideia.com.br>
2021-06-25 13:42:59 -0300

    add exercise 1

---
+++ 03_Day_Booleans_operators_date/03_day_starter/exercises day 3/level_2.js
@@ -0,0 +1,6 @@
+//01- Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+const numberOfBase = 20
+const numberOfHeight = 10
+const areaOfTriangle = (0.5 * numberOfBase * numberOfHeight)
+console.log(`The area of the triangle is ${areaOfTriangle}`)
+
