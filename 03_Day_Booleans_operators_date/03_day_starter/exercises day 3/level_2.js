45de8138f8b1ae1cc7c34a6696f52b7fe58e4039
Kayane Vieira <kayane@startaideia.com.br>
2021-06-25 13:45:16 -0300

    add exercise 2

--- 03_Day_Booleans_operators_date/03_day_starter/exercises day 3/level_2.js
+++ 03_Day_Booleans_operators_date/03_day_starter/exercises day 3/level_2.js
@@ -4,3 +4,9 @@ const numberOfHeight = 10
 const areaOfTriangle = (0.5 * numberOfBase * numberOfHeight)
 console.log(`The area of the triangle is ${areaOfTriangle}`)
 
+//02- Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+const sideA = 20
+const sideB = 10
+const sideC = 5
+const perimeter = (sideA + sideB + sideC);
+console.log(`The perimeter of triangle is: ${perimeter}`)
