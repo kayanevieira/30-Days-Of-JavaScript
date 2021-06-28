8a65d2d6fa4d55eaea099f18123865dceb7e0053
Kayane Vieira <kayane@startaideia.com.br>
2021-06-28 10:01:05 -0300

    fix mistake with exercises prompt

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,27 @@
-// this is your main.js script
+/*01- Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+let numberOfBase = prompt("Número de base?")
+let numberOfHeight = prompt("Número da altura")
+const areaOfTriangle = (0.5 * numberOfBase * numberOfHeight)
+console.log(`The area of the triangle is ${areaOfTriangle}`)*/
+
+//02- Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+/*let sideA = prompt("Side A:")
+let sideB = prompt("Side B:")
+let sideC = prompt("Side C:")
+const perimeter = (sideA + sideB + sideC);
+console.log(`The perimeter of triangle is: ${perimeter}`)*/
+
+/*03- Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+let lengthOf = prompt("What is the length?")
+let width = prompt("What is the widt?")
+const areaRectangle = (lengthOf * width)
+const perimeterRectangle = (2 * (lengthOf + width))
+console.log(`Perimeter of rectangle: ${perimeterRectangle}`)*/
+
+/*04- Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+const radius = prompt("What is the radius")
+const pi = 3.14
+const areaOfCircle = (pi * radius * radius)
+const circumferenceCircle = (2 * pi * radius)
+console.log(`The area of a circle is: ${areaOfCircle}`)
+console.log(`The circumference of a circle is: ${circumferenceCircle}`)*/
