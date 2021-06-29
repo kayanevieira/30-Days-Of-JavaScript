7adafd12c66766c98e98efeb55f6402de04dceaf
Kayane Vieira <kayane@startaideia.com.br>
2021-06-29 13:12:42 -0300

    add exercise 3 and add functions in all

--- 04_Day_Conditionals/04_day_starter/exercises day 4/level_2.js
+++ 04_Day_Conditionals/04_day_starter/exercises day 4/level_2.js
@@ -4,7 +4,7 @@
 60-69, C
 50-59, D
 0-49, F*/
-
+function exercise1(){
 let totalScore = 50
 if (totalScore >= 80 && totalScore <= 100){
     console.log("A")
@@ -16,15 +16,16 @@ if (totalScore >= 80 && totalScore <= 100){
     console.log("D")
 }else{
     console.log("F")
+    }
 }
-
+exercise1()
 /*02- Check if the season is Autumn, Winter, Spring or Summer.
 If the user input is :
 September, October or November, the season is Autumn.
 December, January or February, the season is Winter.
 March, April or May, the season is Spring
 June, July or August, the season is Summer*/
-
+function exercise2(){
 const month = "July"
 
 if (month == 'September' || month == 'October' || month == 'November'){
@@ -35,4 +36,30 @@ if (month == 'September' || month == 'October' || month == 'November'){
     console.log("It's Spring")
 }else if(month == 'June' || month == 'July' || month == 'August'){
     console.log("It's Summer")
+    }
 }
+exercise2()
+/*Check if a day is weekend day or a working day. Your script will take day as an input.
+    What is the day  today? Saturday
+    Saturday is a weekend.
+
+    What is the day today? saturDaY
+    Saturday is a weekend.
+
+    What is the day today? Friday
+    Friday is a working day.
+
+    What is the day today? FrIDAy
+    Friday is a working day.*/
+function exercise3(){
+let day = 'sunday'
+day = day.toLowerCase()
+if (day == 'saturday' || day == 'sunday'){
+    console.log(`${day} is a weekend`)
+}else if(day === 'monday' || day === 'tuesday' || day == 'wednesday' || day == 'thursday' || day == 'friday'){
+    console.log(`${day} is a working day`)
+}else{
+    console.log('invalid day')
+    }
+}
+exercise3()
