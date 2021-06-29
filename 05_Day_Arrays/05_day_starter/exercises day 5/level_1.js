4fd20f881e189138ba5e1e0ad77f76146731eeba
Kayane Vieira <kayane@startaideia.com.br>
2021-06-29 14:13:44 -0300

    add exercise 2 and 3

--- 05_Day_Arrays/05_day_starter/exercises day 5/level_1.js
+++ 05_Day_Arrays/05_day_starter/exercises day 5/level_1.js
@@ -4,3 +4,17 @@ const arr = []
 console.log(arr)
 }
 exercise1()
+
+//02- Declare an array with more than 5 number of elements
+ const arr = [
+     'javascript',
+     'python',
+     'php',
+     'java',
+     'c#',
+     'cobol'
+ ]
+ console.log(arr)
+
+ //03- Find the length of your array
+ console.log(arr.length)
