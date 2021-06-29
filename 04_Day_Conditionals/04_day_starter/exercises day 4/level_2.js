b1dc7b57318b42cd81be58bd1299704fd4497968
Kayane Vieira <kayane@startaideia.com.br>
2021-06-29 11:37:38 -0300

    add exercise 2

--- 04_Day_Conditionals/04_day_starter/exercises day 4/level_2.js
+++ 04_Day_Conditionals/04_day_starter/exercises day 4/level_2.js
@@ -17,3 +17,22 @@ if (totalScore >= 80 && totalScore <= 100){
 }else{
     console.log("F")
 }
+
+/*02- Check if the season is Autumn, Winter, Spring or Summer.
+If the user input is :
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer*/
+
+const month = "July"
+
+if (month == 'September' || month == 'October' || month == 'November'){
+    console.log("It's Autumn")
+}else if(month == 'December' || month == 'January' || month == 'February'){
+    console.log("It's Winter")
+}else if(month == 'March' || month == 'April' || month == 'May'){
+    console.log("It's Spring")
+}else if(month == 'June' || month == 'July' || month == 'August'){
+    console.log("It's Summer")
+}
