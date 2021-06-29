850b7fb7a88fc8a34491efdade1864a640cc4c83
Kayane Vieira <kayane@startaideia.com.br>
2021-06-29 10:17:41 -0300

    add exercise 1 and new folder

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,16 @@
-// this is your main.js script
+//01- Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he neds to turn 18.
 
-alert('Open the browser console whenever you work on JavaScript')
+/*Enter your age: 30
+You are old enough to drive.
+
+Enter your age:15
+You are left with 3 years to drive.*/
+
+const age = prompt("Enter your age: ")
+const yearsToBeAllowed = (18 - age)
+
+if (age >= 18){
+    console.log(`You're ${age}. You are old enough to drive`)
+}else{
+    console.log(`You're ${age}. You will be allowed to drive after ${yearsToBeAllowed} years`)
+}
