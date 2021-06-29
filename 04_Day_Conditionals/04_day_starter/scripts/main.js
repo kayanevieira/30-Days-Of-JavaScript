2204343aa82fc2a0037750d7ce5e7d7a8a42af14
Kayane Vieira <kayane@startaideia.com.br>
2021-06-29 10:40:33 -0300

    add exercise 2

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -5,7 +5,7 @@ You are old enough to drive.
 
 Enter your age:15
 You are left with 3 years to drive.*/
-function exerciseOne(){
+/*function exerciseOne(){
     const age = prompt("Enter your age: ")
     const yearsToBeAllowed = (18 - age)
 
@@ -15,4 +15,25 @@ function exerciseOne(){
     console.log(`You're ${age}. You will be allowed to drive after ${yearsToBeAllowed} years`)
 }
 }
-exerciseOne()
+exerciseOne()*/
+
+//02- Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+//Enter your age: 30
+//You are 5 years older than me.
+/*function secondExercise(){
+const myAge = 18
+const yourAge = prompt("Enter your age: ")
+const yearsOlderMyAge = (myAge - yourAge)
+const yearsOlderYourAge = (yourAge - myAge)
+if (myAge > yourAge){
+    console.log(`I am ${yearsOlderMyAge} years older than you`)
+}else if(myAge < yourAge){
+    console.log(`You are ${yearsOlderYourAge} years older than me`)
+}else{
+    console.log(`We have the same age!`)
+}
+}
+secondExercise()*/
+
+    
