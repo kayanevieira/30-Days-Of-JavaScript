fca0c4170da1c05254d9b64092f1289a7f26224e
Kayane Vieira <kayane@startaideia.com.br>
2021-07-01 10:38:05 -0300

    add exercises

--- 05_Day_Arrays/05_day_starter/exercises day 5/level_3.js
+++ 05_Day_Arrays/05_day_starter/exercises day 5/level_3.js
@@ -1,21 +1,34 @@
 /*01- he following is an array of 10 students ages:
 
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-Sort the array and find the min and max age
-Find the median age(one middle item or two middle items divided by two)
-Find the average age(all items divided by number of items)
-Find the range of the ages(max minus min)
+01- Sort the array and find the min and max age
+02- Find the median age(one middle item or two middle items divided by two)
+03- Find the average age(all items divided by number of items)
+04- Find the range of the ages(max minus min)
 Compare the value of (min - average) and (max - average), use abs() method*/
 
+//01
 let ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 console.log(ages.sort())
-function exercise7(){
-let biggestNumber = 0
+var min = Math.min(...ages)
+var max = Math.max(...ages)
+//02
+let medianNumbers = ages.slice(4,6)
+const result = (parseInt(medianNumbers) + parseInt(medianNumbers)) / 2
+console.log(result)
 
-for(let i = 0; i < ages.length; i++){
-    if (ages[i] > biggestNumber){
-        biggestNumber = ages[i]
-    }
-    
+//03
+var sum = 0;
+for(var i = 0; i < ages.length; i++) {
+    sum += ages[i];
 }
-console.log(biggestNumber)
+let totalAverage = console.log(sum / ages.length)
+//04
+let minNumber = ages.splice(1, 1)
+const maxNumber = ages.splice(-1)
+const numberRage = maxNumber - minNumber
+console.log(numberRage)
+
+//05
+
+
